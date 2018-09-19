84e9be59592b083ac320c962a546f9e263cea5e4
Gregory Kan <kan.gregory@gmail.com>
2018-09-19 17:23:47 +1200

    feat(suppliers): added supplier routes and controller

---
+++ app/controllers/api/v1/suppliers_controller.rb
@@ -0,0 +1,45 @@
+module Api::V1
+  class SuppliersController < ApiController
+    before_action :set_supplier, only: [:show, :update, :destroy]
+
+    # GET /suppliers
+    def index
+      @suppliers = Supplier.all
+      render :json => @suppliers
+    end
+
+    # GET /suppliers/:id
+    def show
+      render :json => @supplier
+    end
+
+    # POST /suppliers
+    def create
+      @supplier.create!(supplier_params)
+      render :json => @supplier
+    end
+
+    # PUT /suppliers/:id
+    def update
+      @supplier.update(supplier_params)
+      head :no_content
+    end
+
+    # DELETE /suppliers/:id
+    def destroy
+      @supplier.destroy
+      head :no_content
+    end
+
+    private
+
+    def supplier_params
+      params.permit(:name)
+    end
+
+    def set_supplier
+      @supplier = Supplier.find(params[:id])
+    end
+
+  end
+end
