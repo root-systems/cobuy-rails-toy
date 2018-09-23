f85d67a52b5ca4a2af605bf77f145fb27b44d741
Gregory Kan <kan.gregory@gmail.com>
2018-09-23 17:31:44 +1200

    feat(products): added action and state to add new products

--- client/src/bundles/products.js
+++ client/src/bundles/products.js
@@ -28,6 +28,7 @@ const bundle = createAsyncResourceBundle({
 })
 
 const initialState = {
+  newProducts: {},
   // needed by createAsyncResourceBundle
   data: null,
   errorTimes: [],
@@ -41,51 +42,20 @@ const initialState = {
 
 const baseReducer = bundle.reducer
 bundle.reducer = (state = initialState, action) => {
-  // if (action.type === 'UPDATE_SUPPLIER_NAME_FIELD') {
-  //   return {
-  //     ...state,
-  //     nameField: action.payload
-  //   }
-  // }
-  // if (action.type === 'CREATE_SUPPLIER_START') {
-  //   return {
-  //     ...state,
-  //     isCreatingSupplier: true
-  //   }
-  // }
-  // if (action.type === 'CREATE_SUPPLIER_SUCCESS') {
-  //   return {
-  //     ...state,
-  //     isCreatingSupplier: false,
-  //     data: concat(state.data, action.payload),
-  //     nameField: ''
-  //   }
-  // }
-  // if (action.type === 'CREATE_SUPPLIER_ERROR') {
-  //   return {
-  //     ...state,
-  //     isCreatingSupplier: false
-  //   }
-  // }
-  // if (action.type === 'UPDATE_SUPPLIER_START') {
-  //   return {
-  //     ...state,
-  //     isUpdatingSupplier: true
-  //   }
-  // }
-  // if (action.type === 'UPDATE_SUPPLIER_SUCCESS') {
-  //   return {
-  //     ...state,
-  //     isUpdatingSupplier: false,
-  //     data: concat(filter(state.data, (supplier) => { return supplier.id !== action.payload.id }), action.payload)
-  //   }
-  // }
-  // if (action.type === 'UPDATE_SUPPLIER_ERROR') {
-  //   return {
-  //     ...state,
-  //     isUpdatingSupplier: false
-  //   }
-  // }
+  if (action.type === 'ADD_NEW_PRODUCT') {
+    const nextId = cuid()
+    return {
+      ...state,
+      newProducts: {
+        ...state.newProducts,
+        [nextId]: {
+          name: '',
+          description: '',
+          unit: ''
+        }
+      }
+    }
+  }
 
   if (action.type === 'SIGN_OUT_SUCCESS') {
     return initialState
@@ -95,6 +65,7 @@ bundle.reducer = (state = initialState, action) => {
 }
 
 bundle.selectProducts = state => state.products.data
+bundle.selectNewProducts = state => state.products.newProducts
 bundle.selectThisSupplierProducts = createSelector(
   'selectThisSupplierId',
   'selectProducts',
@@ -104,65 +75,9 @@ bundle.selectThisSupplierProducts = createSelector(
   }
 )
 
-// bundle.doCreateSupplier = (formData) => ({ dispatch, apiFetch, getState }) => {
-//   const credentials = getState().accounts.credentials
-//   const sanitizedCredentials = {
-//     'access-token': credentials.accessToken,
-//     'token-type': credentials.tokenType,
-//     client: credentials.client,
-//     uid: credentials.uid,
-//     expiry: credentials.expiry
-//   }
-//   dispatch({ type: 'CREATE_SUPPLIER_START' })
-//   apiFetch('api/v1/suppliers', {
-//     method: 'POST',
-//     body: JSON.stringify(formData),
-//     headers: sanitizedCredentials
-//   })
-//     .then(response => {
-//       if (!response.ok) {
-//         return Promise.reject(new Error(`${response.status} ${response.statusText}`))
-//       }
-//       return response.json()
-//     })
-//     .then((data) => {
-//       dispatch({ type: 'CREATE_SUPPLIER_SUCCESS', payload: data })
-//       dispatch({ actionCreator: 'doUpdateHash', args: ['suppliers'] })
-//     })
-//     .catch((error) => {
-//       dispatch({ type: 'CREATE_SUPPLIER_ERROR', payload: error })
-//     })
-// }
-//
-// bundle.doUpdateSupplier = (formData) => ({ dispatch, apiFetch, getState }) => {
-//   const credentials = getState().accounts.credentials
-//   const sanitizedCredentials = {
-//     'access-token': credentials.accessToken,
-//     'token-type': credentials.tokenType,
-//     client: credentials.client,
-//     uid: credentials.uid,
-//     expiry: credentials.expiry
-//   }
-//   dispatch({ type: 'UPDATE_SUPPLIER_START' })
-//   apiFetch(`api/v1/suppliers/${formData.id}`, {
-//     method: 'PATCH',
-//     body: JSON.stringify(omit(formData, ['id'])),
-//     headers: sanitizedCredentials
-//   })
-//     .then(response => {
-//       if (!response.ok) {
-//         return Promise.reject(new Error(`${response.status} ${response.statusText}`))
-//       }
-//       return response.json()
-//     })
-//     .then((data) => {
-//       dispatch({ type: 'UPDATE_SUPPLIER_SUCCESS', payload: data })
-//       dispatch({ actionCreator: 'doUpdateHash', args: ['suppliers'] })
-//     })
-//     .catch((error) => {
-//       dispatch({ type: 'UPDATE_SUPPLIER_ERROR', payload: error })
-//     })
-// }
+bundle.doAddNewProduct = () => ({ dispatch }) => {
+  dispatch({ type: 'ADD_NEW_PRODUCT' })
+}
 
 bundle.reactProductsFetch = createSelector(
   'selectProductsShouldUpdate',
