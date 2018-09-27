debc49fc150a877343131a5bb4f490d3fcd83f20
Gregory Kan <kan.gregory@gmail.com>
2018-09-27 13:54:49 +1200

    feat(wants): added disabled column to wants for soft delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_25_211233) do
+ActiveRecord::Schema.define(version: 2018_09_27_015358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 2018_09_25_211233) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "order_id"
+    t.boolean "disabled", default: false
     t.index ["order_id"], name: "index_wants_on_order_id"
     t.index ["price_spec_id"], name: "index_wants_on_price_spec_id"
     t.index ["product_id"], name: "index_wants_on_product_id"
