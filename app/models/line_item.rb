e56fc27f6cd503c6ea61f71b1ae148801ab97155
Gregory Kan <kan.gregory@gmail.com>
2018-09-27 15:13:56 +1200

    feat(lineItems): added some class methods to fetch related data

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -4,7 +4,9 @@ class LineItem < ApplicationRecord
 
   def self.recompute (order_id, product_ids)
     product_ids.each do |product_id|
-      line_item = LineItem.where(order_id: order_id, product_id: product_id)
+      line_item = self.get_related_line_item
+      wants = self.get_related_wants(order_id, product_id)
+      price_specs = self.get_related_price_specs(product_id)
       if line_item.present?
         puts "a line item exists for this product and order"
         # GK: TODO: logic here
@@ -13,5 +15,17 @@ class LineItem < ApplicationRecord
         # GK: TODO: logic here
       end
     end
+  end
+
+  def self.get_related_wants (order_id, product_id)
+    Want.where(order_id: order_id, product_id: product_id, disabled: false)
+  end
+
+  def self.get_related_price_specs (product_id)
+    PriceSpec.where(product_id: product_id)
+  end
+
+  def self.get_related_line_item (order_id, product_id)
+    LineItem.where(order_id: order_id, product_id: product_id).first
   end
 end
