8c8cf1968804e2d7321916627ced039832a89caa
Gregory Kan <kan.gregory@gmail.com>
2018-09-28 13:35:07 +1200

    feat(lineItems): scaffolding logic

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -9,10 +9,16 @@ class LineItem < ApplicationRecord
       price_specs = self.get_related_price_specs(product_id)
       if line_item.present?
         puts "a line item exists for this product and order"
-        # GK: TODO: logic here
+        if wants.none?
+          puts "wants no longer exist for this line item, so destroy it"
+          line_item.destroy!
+          return
+        end
+        self.aggregate(line_item, wants, price_specs)
       else
-        puts "a line item doesn't yet exist for this product and order"
-        # GK: TODO: logic here
+        puts "a line item doesn't yet exist for this product and order, creating one"
+        new_line_item = LineItem.create!(order_id: order_id, product_id: product_id)
+        self.aggregate(new_line_item, wants, price_specs)
       end
     end
   end
@@ -27,5 +33,9 @@ class LineItem < ApplicationRecord
 
   def self.get_related_line_item (order_id, product_id)
     LineItem.where(order_id: order_id, product_id: product_id).first
+  end
+
+  def self.aggregate (line_item, wants, price_specs)
+    # GK: TODO: aggregation logic here
   end
 end
