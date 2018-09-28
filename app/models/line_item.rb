ea5d399d16a5e589d7d09fff3b85f9a01fc8aea2
Gregory Kan <kan.gregory@gmail.com>
2018-09-28 20:28:10 +1200

    feat(lineItems): rewrote line item recomputation

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -4,67 +4,30 @@ class LineItem < ApplicationRecord
 
   def self.recompute (order_id, product_ids)
     product_ids.each do |product_id|
-      line_item = self.get_related_line_item(order_id, product_id)
-      wants = self.get_related_wants(order_id, product_id)
       price_specs = self.get_related_price_specs(product_id)
-      if line_item.present?
-        puts "a line item exists for this product and order"
-        if wants.none?
-          puts "wants no longer exist for this line item, so destroy it"
-          line_item.destroy!
-          return
+      price_specs.each do |price_spec|
+        line_item = LineItem.where(price_spec_id: price_spec.id, product_id: product_id, order_id: order_id, price_per_unit: price_spec.price).first_or_create
+        wants = Want.where(price_spec_id: price_spec.id, disabled: false)
+        if wants.any?
+          self.aggregate(line_item, wants, price_spec)
+        else
+          line_item.update_attributes(quantity: 0, total_price: 0, minimum_achieved: false)
         end
-        self.aggregate(line_item, wants, price_specs)
-      else
-        if wants.none?
-          puts "no related line item exists and no wants exist so do nothing"
-          return
-        end
-        puts "a line item doesn't yet exist for this product and order, creating one"
-        new_line_item = LineItem.create!(order_id: order_id, product_id: product_id)
-        self.aggregate(new_line_item, wants, price_specs)
       end
     end
-  end
-
-  def self.get_related_wants (order_id, product_id)
-    Want.where(order_id: order_id, product_id: product_id, disabled: false)
   end
 
   def self.get_related_price_specs (product_id)
     PriceSpec.where(product_id: product_id)
   end
 
-  def self.get_related_line_item (order_id, product_id)
-    LineItem.where(order_id: order_id, product_id: product_id).first
-  end
-
-  def self.aggregate (line_item, wants, price_specs)
-    aggregated_wants_per_price_spec = price_specs.map do |price_spec|
-      related_wants = wants.select { |w| w.price_spec_id == price_spec.id }
-      total_quantity_wanted = related_wants.reduce(0) do |sofar, related_want|
-        sofar + related_want.quantity
-      end
-      minimum_achieved = total_quantity_wanted > price_spec.minimum
-      { price: price_spec.price,
-        total_quantity_wanted: total_quantity_wanted,
-        price_spec_id: price_spec.id,
-        minimum_achieved: minimum_achieved }
+  def self.aggregate (line_item, wants, price_spec)
+    total_quantity_wanted = wants.reduce(0) do |sofar, want|
+      sofar + want.quantity
     end
-    achieved_aggregated_wants_per_price_spec = aggregated_wants_per_price_spec.select { |x| x[:minimum_achieved] == true }
-    # GK: TODO: what if minimum is not achieved ?
-    lowest_priced = achieved_aggregated_wants_per_price_spec.reduce do |last, current|
-      if last[:price] < current[:price]
-        last
-      else
-        current
-      end
-    end
-    line_item.quantity = lowest_priced[:total_quantity_wanted]
-    line_item.price_per_unit = lowest_priced[:price]
-    line_item.total_price = lowest_priced[:total_quantity_wanted] * lowest_priced[:price]
-    line_item.price_spec_id = lowest_priced[:price_spec_id]
-    line_item.save!
+    minimum_achieved = price_spec.minimum <= total_quantity_wanted
+    total_price = total_quantity_wanted * price_spec.price
+    line_item.update_attributes(quantity: total_quantity_wanted, total_price: total_price, minimum_achieved: minimum_achieved)
   end
 
 end
